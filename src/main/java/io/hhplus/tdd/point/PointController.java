02cd1f3e29f9cea589d01840c697e94c28a5788c
haeraenii <haeryun0917@gmail.com>
2024-06-18 22:11:46 +0900

    테스트코드 작성

---
+++ src/main/java/io/hhplus/tdd/point/PointController.java
@@ -0,0 +1,75 @@
+package io.hhplus.tdd.point;
+
+import io.hhplus.tdd.database.PointHistoryTable;
+import io.hhplus.tdd.database.UserPointTable;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/point")
+public class PointController {
+
+    private static final Logger log = LoggerFactory.getLogger(PointController.class);
+
+    private final UserPointTable userPointTable;
+
+    private final PointHistoryTable pointHistoryTable;
+
+    /**
+     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     */
+    @GetMapping("{id}")
+    public UserPoint point(
+            @PathVariable long id
+    ) {
+        return userPointTable.selectById(id);
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+     */
+    @GetMapping("{id}/histories")
+    public List<PointHistory> history(
+            @PathVariable long id
+    ) {
+        return pointHistoryTable.selectAllByUserId(id);
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+     */
+    @PatchMapping("{id}/charge")
+    public UserPoint charge(
+            @PathVariable long id,
+            @RequestBody long amount
+    ) {
+        pointHistoryTable.insert(id, amount, TransactionType.CHARGE, System.currentTimeMillis());
+        return userPointTable.insertOrUpdate(id, amount);
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
+     */
+    @PatchMapping("{id}/use")
+    public UserPoint use(
+            @PathVariable long id,
+            @RequestBody long amount
+    ) {
+        UserPoint userPoint = userPointTable.selectById(id);
+
+        if(userPoint.point() < amount) {
+            log.info("현재 잔여 포인트는" + userPoint.point() + "입니다.");
+
+            return null;
+        } else {
+            pointHistoryTable.insert(id, amount, TransactionType.USE, System.currentTimeMillis());
+
+            return userPointTable.insertOrUpdate(id, userPoint.point() - amount);
+        }
+    }
+}
---
+++ src/test/java/io/hhplus/tdd/TddApplicationTest.java
@@ -0,0 +1,10 @@
+package io.hhplus.tdd;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class TddApplicationTest {
+}
